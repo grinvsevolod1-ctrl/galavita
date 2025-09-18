94fbc31b8123d8d78b708a5c0249db8bb41e895e
Всеволод <grinvsevolod1@netnext.site>
2025-09-18 16:18:16 +0300

    123

--- src/layouts/components/MenuButton.tsx
+++ src/layouts/components/MenuButton.tsx
@@ -1,6 +1,11 @@
 import { IoMenu } from "react-icons/io5";
+import type { FC } from "react";
 
-const MenuButton = ({ onClick }) => (
+interface Props {
+  onClick: () => void;
+}
+
+const MenuButton: FC<Props> = ({ onClick }) => (
   <button
     className="lg:hidden p-2 text-xl text-text-dark dark:text-white"
     onClick={onClick}
